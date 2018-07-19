dbb7643758226fd38bf544cb6324ee6ffb70fdd1
Mario Quintana <pyrros2000@gmail.com>
2018-07-19 11:03:58 -0700

    adding stop test not passing getting 404

--- test/e2e/tours.test.js
+++ test/e2e/tours.test.js
@@ -2,6 +2,11 @@ const { assert } = require('chai');
 const request = require('./request');
 const { dropCollection } = require('./db');
 
+const checkOk = res => {
+    assert.equal(res.status, 200, 'Expected 200 http status code');
+    return res;
+};
+
 describe('Tour API', () => {
 
     beforeEach(() => dropCollection('tours'));
@@ -65,7 +70,6 @@ describe('Tour API', () => {
                 assert.equal(body[0].title, ['Grand Tour']);
                 assert.equal(body[1].title, ['Red Tour']);
             });
-
     });
 
     it('Gets a tour by Id', () => {
@@ -73,6 +77,34 @@ describe('Tour API', () => {
             .get(`/api/tours/${tour._id}`)
             .then(({ body }) => {
                 assert.deepEqual(body, tour);
+            });
+    });
+
+    function addStop(title, stop) {
+        return request
+            .post(`/api/tours/${tour._id}/stops`)
+            .send(stop)
+            .then(checkOk)
+            .then(({ body }) => body);
+    }
+
+    it('Adds a stop to a tour', () => {
+        const burningMan = {
+            location: {
+                city: 'El Paso',
+                state: 'Texas',
+                zip: 80514
+            },
+            weather: {
+                temperature: 80
+            },
+            attendance: 100
+        };
+
+        return addStop(tour, burningMan)
+            .then(stop => {
+                assert.isDefined(stop._id);
+                assert.equal(stop.title, burningMan.title);
             });
     });
 
