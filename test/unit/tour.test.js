7e4ec5a344473ea81bb6a3b76cebeb4e9129d55d
Mario Quintana <pyrros2000@gmail.com>
2018-07-18 19:17:44 -0700

    Model valid test passing but still need launchDate and weather?

--- test/unit/tour.test.js
+++ test/unit/tour.test.js
@@ -1,3 +1,28 @@
-const { assert } = require('chai');
-// const { getErrors } = require('./helpers');
+const chai = require('chai');
+const { assert } = chai;
 const Tour = require('../../lib/models/tour');
+
+describe('Tour model', () => {
+
+    it('Validates good model', () => {
+        const data = {
+            title: 'Grand Tour',
+            activities: ['racing', 'drifting', 'time trail'],
+            // launchDate: Date,
+            stops: [{
+                city: 'Portland',
+                state: 'Oregon',
+                zip: 97020,
+                weather: 'rain',
+                attendance: 2
+            }]
+        };
+
+        const tour = new Tour(data);
+        const json = tour.toJSON();
+        delete json._id;
+        json.stops.forEach(s => delete s._id);
+        assert.deepEqual(json, data);
+        assert.isUndefined(tour.validateSync());
+    });
+});
