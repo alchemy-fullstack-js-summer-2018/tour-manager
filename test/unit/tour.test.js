09e7418945c6f7a1de0efee754b11b3b7ac84e72
Arthur Jen <arthurljen@gmail.com>
2018-07-18 17:29:35 -0700

    completed good model and required title validation

---
+++ test/unit/tour.test.js
@@ -0,0 +1,53 @@
+const { assert } = require('chai');
+const { getErrors } = require('./helpers');
+const Tour = require('../../lib/models/tour');
+
+describe('Tour model', () => {
+
+    it('validates good model', () => {
+        const data = {
+            title: 'Tables & Chairs',
+            activities: ['pony rides', 'dancing bears', 'band'],
+            launchDate: new Date(2019, 2, 18),
+            stops: [
+                {
+                    location: {
+                        city: 'Honolulu',
+                        state: 'HI',
+                        zip: '96813'
+                    },
+                    weather: {
+                        temperature: 82,
+                        condition: 'cloudy'
+                    },
+                    attendance: 1800
+                },
+                {
+                    location: {
+                        city: 'Portland',
+                        state: 'OR',
+                        zip: '97212'
+                    },
+                    weather: {
+                        temperature: 92,
+                        condition: 'sunny'
+                    },
+                    attendance: 600
+                }
+            ]
+        };
+
+        const tour = new Tour(data);
+        const json = tour.toJSON();
+        delete json._id;
+        json.stops.forEach(s => delete s._id);
+        assert.deepEqual(json, data);
+        assert.isUndefined(tour.validateSync());
+    });
+
+    it('validates title is required', () => {
+        const tour = new Tour({});
+        const errors = getErrors(tour.validateSync(), 1);
+        assert.equal(errors.title.kind, 'required');
+    });
+});
