836428d8f2f7deca5a00a5ca5e4c7ff6e4cdd3db
rbtprograms <rbt61690@gmail.com>
2018-07-19 10:08:06 -0700

    first e2e test set up

--- test/e2e/tours.test.js
+++ test/e2e/tours.test.js
@@ -1,3 +1,32 @@
 const { assert } = require('chai');
 const request = require('./request');
-const {dropCollection } = require('./db');
+const { dropCollection } = require('./db');
+
+const checkOk = res => {
+    assert.equal(res.status, 200, 'expected http 200 status code');
+    return res;
+};
+
+describe('Tours API', () => {
+
+    beforeEach(() => dropCollection('tours'));
+
+    function save(tour) {
+        return request
+            .post('/api/tours')
+            .send(tour)
+            .then(checkOk)
+            .then(({ body }) => body);
+    }
+    let tour;
+    beforeEach(() => {
+        return save({ title: 'Amazing Tour' })
+            .then(data => {
+                tour = data;
+            });
+    });
+
+    it('saves a tour', () => {
+        assert.isOk(tour._id);
+    });
+});
