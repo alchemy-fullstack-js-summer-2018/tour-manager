3643892cea51697565b15ec5880e27a93ac96cbc
MariahAdams <mariah.adams23@yahoo.com>
2018-07-19 09:20:54 -0700

    test for min attendance passes

--- test/unit/tours.test.js
+++ test/unit/tours.test.js
@@ -49,6 +49,36 @@ describe('Tour model', () => {
     it('validates that title is required', () => {
         const tour = new Tour({});
         const errors = getErrors(tour.validateSync(), 1);
+
+        assert.equal(Object.keys(errors).length, 1);
+        assert.equal(errors.title.kind, 'required');
+    });
+
+    it('validates that min attendance for stops is 1', () => {
+        const tour = new Tour({
+            title: 'Being for the Benefit of Mr.Kite!',
+            stops: [{
+                location: {
+                    city: 'Portland',
+                    state: 'Oregon',
+                    zip: 92115
+                },
+                weather: {
+                    temperature: 68,
+                    condition: 'rainy'
+                },
+                attendance: 0
+            }]
+        });
+
+        const errors = getErrors(tour.validateSync(), 1);
+        
+        assert.equal(Object.keys(errors).length, 1);
+        assert.equal(errors['stops.0.attendance'].kind, 'min');
+    });
+
+    it('validates that the date defaults to now', () => {
+
     });
 
 });
