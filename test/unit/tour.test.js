07c8149eeff9e293c007368ae4a12b8bb6a2dd02
Robyn Navarro <rnavarro182@gmail.com>
2018-07-18 18:17:13 -0700

    validates good model test passed

---
+++ test/unit/tour.test.js
@@ -0,0 +1,32 @@
+const { assert } = require('chai');
+const { getErrors } = require('./helpers');
+const Tour = require('../../lib/models/tour');
+
+describe('Pirate model', () => {
+    it('validates good model', () => {
+        const data = {
+            name: 'Cirque du Soleil',
+            activities: ['contortion', 'trapeze'],
+            lunchDate: new Date(),
+            stops: [{
+                location: {
+                    city: 'Portland',
+                    state: 'Oregon',
+                    zip: 97209
+                },
+                weather: {
+                    temperature: 75,
+                    condition: 'Partially cloudy'
+                },
+                attendance: 1
+            }]
+        };
+        const tour = new Tour(data);
+
+        const json = tour.toJSON();
+        json.stops.forEach(s => delete s._id);
+        delete json._id;
+        assert.deepEqual(json, data);
+        assert.isUndefined(tour.validateSync());
+    });
+});
