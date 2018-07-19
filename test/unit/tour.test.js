fec11e74fa1c5ec73fb5bdaa5f006343f7812cd8
Antreo Pukay <acrosswalker@yahoo.com>
2018-07-19 09:10:54 -0700

    good model test passing

--- test/unit/tour.test.js
+++ test/unit/tour.test.js
@@ -7,25 +7,26 @@ describe('Tour model', () => {
     it('validates good model', () => {
         const data = {
             title: 'Amazers',
-            activities: 'trapeze',
-            launchDate: Date,
+            activities: ['trapeze', 'lions', 'clowns'],
+            launchDate: new Date,
             stops: [{
                 location: {
                     city: 'Portland',
                     state: 'OR',                        
                     zip: 97203,
                 },
-                weather: 97203,
+                weather: {
+                    temperature: 80,
+                },
                 attendance: 1000,
-            }],
+            }]
         };
-
         const tour = new Tour(data);
         
         const json = tour.toJSON();
         delete json._id;
         console.log('** tour validate **', tour);
-        // json.stops.forEach(s => delete s._id);
+        json.stops.forEach(s => delete s._id);
         assert.deepEqual(json, data);
         // assert.isUndefined(tour.validateSync());
     });
@@ -33,7 +34,7 @@ describe('Tour model', () => {
     it('title is required', () => {
         const tour = new Tour({});
         console.log('** tour **', tour);
-        const errors = getErrors(tour.validateSync(), 5);
+        const errors = getErrors(tour.validateSync(), 1);
         assert.equal(errors.title.kind, 'required');  
     });
 
