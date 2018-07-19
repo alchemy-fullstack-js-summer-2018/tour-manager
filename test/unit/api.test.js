fe860034b2f63b6f79d6e88faae662a6d4bd6885
rbtprograms <rbt61690@gmail.com>
2018-07-18 23:35:40 -0700

    data is stuck in promises

--- test/unit/api.test.js
+++ test/unit/api.test.js
@@ -1,45 +1,53 @@
 const { assert } = require('chai');
 
-function createMiddleware(api) {
-    return (req, res, next) => {
-        const data = api(req.body.zip);
-        console.log('**API**', data);
-        console.log('**REQ**', req);
-        console.log('**RES**', res);
-        console.log('**NEXT**', next);
-        next();
-    };
-}
 
-describe('the weather API', done => {
-    const location = {
-        city: 'Portland',
-        state: 'Oregon'
-    };
-    const weather = {
-        temperature: 83,
-        condition: 'Windy'
-    };
+describe('the weather API', () => {
+    function createMiddleware(api) {
+        return (req, res, next) => {
+            req.body = api(req.body.zip);
+            // console.log('**DATA**', data);
+            console.log('**REQ**', req);
+            // console.log('**RES**', res);
+            // console.log('**NEXT**', next);
+    
+            next();
+        };
+    }
+    it('gets the location and weather info for a given zip', done => {
 
-    const api = zip => {
-        assert.equal(zip, '97205');
-        return Promise.resolve({
-            weather, location
-        });
-    };
-
-    const middleware = createMiddleware(api);
-
-    const next = () => {
-        assert.deepEqual(weather, data);
-        done();
-    };
-
-    const req = {
-        body: {
-            zip: '97205'
-        }
-    };
-
-    middleware(req, null, next);
+        const location = {
+            city: 'Portland',
+            state: 'Oregon'
+        };
+        const weather = {
+            temperature: 83,
+            condition: 'Windy'
+        };
+    
+        const api = zip => {
+            assert.equal(zip, '97205');
+            return Promise.resolve({
+                weather, location
+            });
+        };
+    
+        
+        const next = () => {
+            console.log('**IN NEXT**', req.body);
+            assert.deepEqual(weather, req);
+            done();
+        };
+        const middleware = createMiddleware(api);
+        
+        const req = {
+            body: {
+                zip: '97205'
+            }
+        };
+    
+        middleware(req, null, next);
+    
+        // console.log('***LOWER MOST CONSOLE***', req.body);
+        // assert.deepEqual(weather, req.body);
+    });
 });
