aa461bbe5ec0ca33874de959f44073a7deb45761
MariahAdams <mariah.adams23@yahoo.com>
2018-07-18 17:26:20 -0700

    file cleanup, starting good model validation

--- test/unit/tours.test.js
+++ test/unit/tours.test.js
@@ -6,7 +6,39 @@ describe('Tour model', () => {
 
     it('validates good model', () => {
         const data = {
-            // example full, good data
+            title: {
+                type: 'P.T. Barnum\'s Circus at London',
+            },
+            activities: ['tightrope', 'trapeze', 'lion tamer'],
+            launchDate: {
+                type: 'July 18th, 2018',
+            },
+            stops: [
+                {
+                    location: {
+                        city: 'London',
+                        state: 'California',
+                        zip: 92115
+                    },
+                    weather: {
+                        temperature: 76,
+                        condition: 'sunny'
+                    },
+                    attendance: 100
+                },
+                {
+                    location: {
+                        city: 'San Diego',
+                        state: 'California',
+                        zip: 92115
+                    },
+                    weather: {
+                        temperature: 75,
+                        condition: 'sunny'
+                    },
+                    attendance: 400
+                }
+            ]
         };
         const tour = new Tour(data);
 
