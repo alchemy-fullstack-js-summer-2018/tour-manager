fbf28850d1bbbc5bc67bcac9b84962219a3ed19b
MariahAdams <mariah.adams23@yahoo.com>
2018-07-19 11:20:03 -0700

    middleware setup and async test passing

--- test/unit/middleware.test.js
+++ test/unit/middleware.test.js
@@ -1,3 +1,55 @@
 const { assert } = require('chai');
-const { middleware } = require('../../lib/utils/middleware');
-const { getErrors } = require('./helpers');
+const { createMiddleware } = require('../../lib/utils/middleware');
+
+describe.only('Middleware tests', () => {
+
+    it.only('handles async', done => {
+        const weather = {
+            temperature: '',
+            condition: ''
+        };
+        const location = {
+            city: 'San Diego',
+            state: 'California'
+        };
+
+        const api = zip => {
+            assert.equal(zip, '92115');
+            return Promise.resolve({
+                weather, location
+            });
+        };
+
+        const middleware = createMiddleware(api);
+
+        const next = () => {
+            //make assertions
+            //test that req.body has weather and location
+            assert.deepEqual(req.body, { weather, location });
+
+            done();
+        };
+
+        const req = {
+            query: {
+                zip: '92115'
+            },
+            body: {
+                weather: {
+                    temperature: '',
+                    condition: ''
+                },
+                location: {
+                    city: 'San Diego',
+                    state: 'California',
+                }
+            }
+        };
+
+        middleware(req, null, next);
+    });
+
+    it('takes a zip when a stop is added', () => {
+
+    });
+});
