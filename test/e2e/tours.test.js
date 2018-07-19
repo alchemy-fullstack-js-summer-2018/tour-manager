9135b41269326741b0808778d5fb7d6a6363758f
Mark Myers <markalope@gmail.com>
2018-07-19 11:41:28 -0700

    got return all tours to pass

---
+++ test/e2e/tours.test.js
@@ -0,0 +1,40 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+
+const checkOk = res => {
+    assert.equal(res.status, 200, 'expected Error 200');
+    return res;
+};
+
+describe('Tours tests', () => {
+    beforeEach(() => dropCollection('tours'));
+    let walrus;
+
+    beforeEach(() => {
+        const data = {
+            title: 'Circus Walrus 2018 Tour',
+            activities: ['trained walruses', 'fire eaters'],
+            launchDate: new Date(2018, 7, 18)
+        };
+        return request 
+            .post('/api/tours')
+            .send(data)
+            .then(checkOk)
+            .then(({ body }) => {
+                assert.deepEqual(body.title, data.title);
+                walrus = body;
+            });
+    });
+
+
+    it('returns ALL tours', () => {
+        return request
+            .get('/api/tours')
+            .then(checkOk)
+            .then(({ body }) => {
+                assert.deepEqual(body, [walrus]);
+            });
+    });
+
+});
