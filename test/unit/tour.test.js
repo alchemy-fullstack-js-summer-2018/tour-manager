9d687b2de11a64740c304362923d03b0daf31b0c
Antreo Pukay <acrosswalker@yahoo.com>
2018-07-18 21:34:22 -0700

    matched classwork package.json

---
+++ test/unit/tour.test.js
@@ -0,0 +1,31 @@
+const { assert } = require('chai');
+const { getErrors } = require('./helpers');
+const Tour = require('../../lib/models/tour');
+
+describe('Tour model', () => {
+
+    it('validates good model', () => {
+        const data = {
+            title: 'Amazers',
+            activities: 'trapeze',
+            launchDate: Date.now(),
+            stops: [{
+                location: {
+                    city: 'Portland',
+                    state: 'OR',                        
+                    zip: 97203,
+                },
+                weather: 97203,
+                attendance: 1000,
+            }],
+        };
+
+        const tour = new Tour(data);
+
+        const json = tour.toJSON();
+        delete json._id;
+        json.stops.forEach(s => delete s._id);
+        assert.deepEqual(json, data);
+        assert.isUndefined(stop.validateSync());
+    });
+});
