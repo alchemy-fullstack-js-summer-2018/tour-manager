ee532ec2ae86a014ff7f07b82c710adc77cf6d78
Arthur Jen <arthurljen@gmail.com>
2018-07-19 12:51:33 -0700

    completed POST stops

--- test/e2e/tours.test.js
+++ test/e2e/tours.test.js
@@ -50,35 +50,29 @@ describe('Tours API', () => {
     it.skip('should GET location/weather data', () => {
         return getLocationWeather('96813')
             .then(data => {
-                console.log(data);
                 assert.isDefined(data);
             });
     });
 
     it('should POST a stop to a tour', () => {
         const data = { zip: '96813' };
-        // const expected = {
-        //     location: {
-        //         city: 'Honolulu',
-        //         state: 'HI',
-        //         zip: '96813'
-        //     },
-        //     weather: {
-        //         temperature: '82',
-        //         condition: 'cloudy'
-        //     }
-        // };
+        const location = {
+            city: 'Honolulu',
+            state: 'HI',
+            zip: '96813'
+        };
         return request
             .post(`/api/tours/${tables._id}/stops`)
             .send(data)
             .then(checkOk)
-            // .then(({ body }) => {
-            //     delete body._id;
-            //     assert.deepEqual(body, data);
-            // });
+            .then(({ body }) => {
+                assert.deepEqual(body.location, location);
+                assert.isDefined(body.weather.temperature);
+                assert.isDefined(body.weather.condition);
+            });
     });
 
-    describe.skip('Tours API - error handler', () => {
+    describe('Tours API - error handler', () => {
 
         it('should throw 404 error on bad path', () => {
             return request
