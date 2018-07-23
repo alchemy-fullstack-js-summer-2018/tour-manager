681b58f8950ef873e310f5741bcfc096cc194a9e
Carmen V Ramos <carmenvramos@yahoo.com>
2018-07-23 16:57:27 -0700

    GET & SAVES Tests Passing

---
+++ test/e2e/tours.test.js
@@ -0,0 +1,39 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+
+const checkOk = res => {
+    assert.equal(res.status, 200, 'expected 200 http status code');
+    return res;
+};
+describe('Tours API', () => {
+    
+    beforeEach(() => dropCollection('tours'));
+
+    let universoul;
+    beforeEach(() => {
+        return request
+            .post('/api/tours')
+            .send({
+                title: 'UniverSoul Circus',
+                activities: ['flying trapeze', 'carnival rides', 'carnival games'],
+                launchDate: new Date(1998, 6, 1)
+            })
+            .then((res) => {
+                universoul = res.body;
+            });
+    });
+
+    it('saves a tour', () => {
+        assert.isOk(universoul._id);
+    });
+
+    it('gets a tour', () => {
+        return request
+            .get('/api/tours/' + universoul._id)
+            .then(checkOk)
+            .then((res) => {
+                assert.deepEqual(res.body._id, universoul._id);
+            });
+    });
+});
