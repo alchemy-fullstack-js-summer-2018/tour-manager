3fb97bee216bcf7d0a5bc5953e124850cd4e35e0
Carmen V Ramos <carmenvramos@yahoo.com>
2018-07-24 12:24:16 -0700

    Add a Stop to Tour Test Passing

--- test/e2e/tours.test.js
+++ test/e2e/tours.test.js
@@ -17,7 +17,7 @@ describe('Tours API', () => {
             .send({
                 title: 'UniverSoul Circus',
                 activities: ['flying trapeze', 'carnival rides', 'carnival games'],
-                launchDate: new Date(1998, 6, 1)
+                launchDate: new Date(1998, 6, 1),
             })
             .then((res) => {
                 universoul = res.body;
@@ -41,8 +41,48 @@ describe('Tours API', () => {
         return request
             .get(`/api/tours/${universoul._id}`)
             .then(({ body }) => {
-                console.log(body);
                 assert.deepEqual(body, universoul);
             });
     });
+
+    function addStop(tour, stop) {
+        return request
+            .post(`/api/tours/${tour._id}/stops`)
+            .send(stop)
+            .then(checkOk)
+            .then(({ body }) => body);
+    }
+
+    it('add a stop to this tour', () => {
+        const jamestown = {
+            location: {
+                city: 'St Louis',
+                state: 'MO',
+                zip: 63034
+            },
+            weather: {
+                temperature: '78 F',
+                condition: 'Sunny'
+            },
+            attendance: 100
+        };
+
+        return addStop(universoul, jamestown)
+            .then(stop => {
+                assert.isDefined(stop._id);    
+                assert.equal(stop.city, jamestown.city);
+            });
+    });
+
+    // it('updates stops with number of attendance', () => {
+    //     const data = { attendance: 1000 };
+    //     return request
+    //         .put(`/api/tours/${tour._id}/stops/${tour.stops[0].id}/attendance`)
+    //         .send(data)
+    //         .then(checkOk)
+    //         . then(({ body }) => {
+    //             assert.deepEqual(body.stops[0].attendance, 1000);
+    //         });
+    // });
+    
 });
