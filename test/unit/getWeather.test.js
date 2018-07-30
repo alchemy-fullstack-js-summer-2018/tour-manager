3d591c0250fd2b81c75432d8ab1f05a249eda5bd
Carmen V Ramos <carmenvramos@yahoo.com>
2018-07-30 05:30:01 -0700

    getWeather Unit Test Passing

--- test/unit/getWeather.test.js
+++ test/unit/getWeather.test.js
@@ -1,14 +1,43 @@
-// const { assert } = require('chai');
-// const getWeather = require('../../lib/uti;/get-weather');
-// const { HttpError } = require('../../lib/util/errors');
+const { assert } = require('chai');
 
-// describe('get weather middleware', () => {
-    
-//     it('factory function returns middleware function with correct param', () => {
-//         const weatherMiddleware = getWeather();
-//         assert.typeOf(weatherMiddleware, 'function');
-//         assert.equal(weatherMiddleware.length, 3);
-//     });
+function createLocationWeather(api) {
+    return (req, res, next) => {
+        return api(req.body.zip)
+            .then(data => {
+                req.body = data;
+                next();
+            });
+    };
+}
 
-//     it('calls next when ')
-// });
+it('test function tha finds weather for a zip', done => {
+    const weather = {
+        temperature: '78 F',
+        condition: 'Sunny'
+    };
+    const location = {
+        city: 'St Louis',
+        state: 'MO',
+        zip: '63034'
+    };
+
+    const getLocationWeather = zip => {
+        assert.equal(zip, '63034');
+        return Promise.resolve({
+            weather, location
+        });
+    };
+
+    const req = {
+        body: { zip: '63034' }
+    };
+
+    const next = () => {
+        assert.deepEqual(req.body.location, location, 'location missing');
+        assert.deepEqual(req.body.weather, weather, 'weather missing');
+        done();
+    };
+
+    const getLocation = createLocationWeather(getLocationWeather);
+    getLocation(req, null, next);
+});
